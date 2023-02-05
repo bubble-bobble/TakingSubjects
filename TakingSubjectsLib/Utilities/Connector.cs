cfc68fcb7deecfc5f83090bda0c29a4d8a0dbdfa
joan-hash <joanrojasp@gmail.com>
2023-02-05 11:05:01 -0300

    - Creación de la cadena de conexión a la base de datos. - Creación de clase para manejar la cadena de conexión transversalmente dentro de la aplicación. - Agregación del contexto de datos, así como también del contexto de procedimientos almacenados.

---
+++ TakingSubjectsLib/Utilities/Connector.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TakingSubjectsLib.Utilities
+{
+    public static class Connector
+    {
+        public static string ConnectionString
+        {
+            get
+            {
+                return ConfigurationManager.ConnectionStrings["AppConnectionString"].ConnectionString;
+            }
+        }
+    }
+}
